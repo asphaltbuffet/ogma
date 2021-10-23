28293133ff82ae509142a6cce9663bfb7e2b0b06
Ben Lechlitner <otherland@gmail.com>
2021-10-23 16:10:00 -0400

    initial pass at db structure with open/close.

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/asphaltbuffet/ogma
 
 go 1.16
 
-require github.com/stretchr/testify v1.7.0
+require (
+	github.com/asdine/storm/v3 v3.2.1
+	github.com/br0xen/boltbrowser v0.0.0-20210531150353-7f10a81cece0 // indirect
+	github.com/stretchr/testify v1.7.0
+)
--- main.go
+++ main.go
@@ -2,9 +2,42 @@
 package main
 
 import "fmt"
+import "github.com/asdine/storm/v3"
+
+// Contains relevant information for LEX listings.
+type Listing struct {
+	ID                  int `storm:"id,increment"`
+	IssueNumber         int
+	PageNumber          int
+	IndexedMemberNumber int    `storm:"index"`
+	IndexedCategory     string `storm:"index"`
+	ListingText         string
+}
 
 func main() {
 	fmt.Println(greet())
+
+	db, err := storm.Open("ogma.db")
+	if err != nil {
+		fmt.Println("Failed to open db: ", err)
+	}
+
+	defer db.Close()
+
+	listing := Listing{
+		ID:                  1,
+		IssueNumber:         56,
+		PageNumber:          1,
+		IndexedMemberNumber: 2989,
+		IndexedCategory:     "Art & Photography",
+		ListingText:         "Fingerpainting exchange.",
+	}
+
+	err = db.Save(&listing)
+	if err != nil {
+		fmt.Println("Failed to save to db: ", err)
+	}
+
 }
 
 func greet() string {
