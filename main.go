ec4a96aa7785b451c742d1d287d3e2485fdba1b3
Ben Lechlitner <otherland@gmail.com>
2021-10-31 16:53:08 -0400

    better parsing of log level from config

--- main.go
+++ main.go
@@ -32,39 +32,9 @@ func PrepConfigAndLogging() {
 		fmt.Fprintln(os.Stderr, "No config file found:", viper.ConfigFileUsed())
 	}
 
-	// Create the log file if doesn't exist. And append to it if it already exists.
-	// f, err := os.OpenFile(viper.GetString("logging.filename"), os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0o600) //nolint:gomnd // setting file permissions
-	// Formatter := new(log.TextFormatter)
-	// You can change the Timestamp format. But you have to use the same date and time.
-	// "2006-02-02 15:04:06" Works. If you change any digit, it won't work
-	// ie "Mon Jan 2 15:04:05 MST 2006" is the reference time. You can't change it
-	// Formatter.TimestampFormat = "2006-01-02 15:04:05"
-	// Formatter.FullTimestamp = true
-	// log.SetFormatter(Formatter)
-	// if err != nil {
-	// Cannot open log file. Logging to stderr
-	//	fmt.Println(err)
-	// } else {
-	//	log.SetOutput(f)
-	// }
-
-	// TODO: Use Parse.LogLevel
-	switch viper.GetString("logging.level") {
-	case "trace":
-		log.SetLevel(log.TraceLevel)
-	case "debug":
-		log.SetLevel(log.DebugLevel)
-	case "info":
-		log.SetLevel(log.InfoLevel)
-	case "warn":
-		log.SetLevel(log.WarnLevel)
-	case "error":
-		log.SetLevel(log.WarnLevel)
-	case "fatal":
-		log.SetLevel(log.FatalLevel)
-	case "panic":
-		log.SetLevel(log.PanicLevel)
-	default:
-		log.SetLevel(log.WarnLevel)
+	loggingLevel, err := log.ParseLevel(viper.GetString("logging.level"))
+	if err != nil {
+		loggingLevel = log.InfoLevel // default to info level logging
 	}
+	log.SetLevel(loggingLevel)
 }
