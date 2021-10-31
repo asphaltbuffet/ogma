3c83b49b027eaae3868146f26ba2ecc903050339
Ben Lechlitner <otherland@gmail.com>
2021-10-31 01:38:00 -0400

    update datastore test assertions

--- pkg/datastore/datastore_test.go
+++ pkg/datastore/datastore_test.go
@@ -15,26 +15,27 @@ func TestManagerNew(t *testing.T) {
 	manager, dbFilePath, err := initDatastoreManager()
 	defer func() {
 		err = os.Remove(dbFilePath)
-		assert.Nil(t, err)
+		assert.NoError(t, err)
 	}()
 	defer manager.Stop()
 
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 
 	_, err = os.Stat(dbFilePath)
 
-	assert.False(t, os.IsNotExist(err))
+	assert.NotErrorIs(t, err, os.ErrNotExist)
 }
 
 func TestManagerNewFail(t *testing.T) {
 	manager, dbFilePath, err := initDatastoreManager()
 	defer func() {
 		err = os.Remove(dbFilePath)
-		assert.Nil(t, err)
+		assert.NoError(t, err)
 	}()
 	defer manager.Stop()
 
 	_, err = datastore.New(dbFilePath)
+	assert.Error(t, err, "datastore manager should fail to open duplicate db file")
 }
 
 func TestManagerGetPath(t *testing.T) {
