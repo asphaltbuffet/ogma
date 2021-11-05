caf17b6b965b501c9c8a0a8925fd91e75fd46749
Ben Lechlitner <otherland@gmail.com>
2021-11-05 00:26:15 -0400

    change rendering to soft wrap columns;

--- pkg/cmd2/listings.go
+++ pkg/cmd2/listings.go
@@ -25,7 +25,7 @@ package cmd
 
 import (
 	"github.com/jedib0t/go-pretty/v6/table"
-	"github.com/spf13/viper"
+	"github.com/jedib0t/go-pretty/v6/text"
 )
 
 // A Listing contains relevant information for LEX listings.
@@ -83,8 +83,9 @@ func RenderListings(ll []Listing) string {
 
 	lt.SetColumnConfigs([]table.ColumnConfig{
 		{
-			Name:     "Text",
-			WidthMax: viper.GetInt("defaults.max_column"),
+			Name:             "Text",
+			WidthMax:         80, //nolint:gomnd // using viper fails unit tests. Fix this 2021-11-04 BL
+			WidthMaxEnforcer: text.WrapSoft,
 		},
 	})
 
